c78d966cddad5a7cd2472cc01517490dedce82ff
João M. M. Leal <j.marcos.mla@gmail.com>
2024-12-24 17:34:28 -0300

    ok

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ const path = require('path');
 const app = express();
 const prisma = new PrismaClient();
 
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+
+// Configuração do express-session
+app.use(
+  session({
+    store: new pgSession({
+      conString: process.env.DATABASE_URL, // URL do seu banco PostgreSQL no Render
+    }),
+    secret: 'sua_chave_secreta_aqui', // Substitua por uma chave segura
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
+      secure: false, // True se usar HTTPS
+      httpOnly: true,
+    },
+  })
+);
+
+
 // Middleware para servir imagens estáticas
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
